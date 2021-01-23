4359e0ef30b2d62b084775d04f881734d66b5b6c
Dakota Maulding <dakota.maulding+gitlab@gmail.com>
2021-01-23 15:23:33 -0600

    Testing with applying to project staticaly

--- src/main/groovy/org/yellolab/KeyringPlugin.groovy
+++ src/main/groovy/org/yellolab/KeyringPlugin.groovy
@@ -3,6 +3,7 @@ package org.yellolab
 import org.gradle.api.Plugin
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.yellolab.Keyring;
 
 /**
  * TODO: Add examples here for using in both settings and project scope.
@@ -14,18 +15,31 @@ class KeyringPlugin implements Plugin {
 
     @Override
     void apply(Object o) {
-
+        //Use this one with the keyring::method in the build scripts
+        def keyring = new KeyringPlugin();
     }
 
+    //or use it with the imported class by using Class.method with these
     public static String getSecret(String host, String userName) {
         logger.info("Retrieving secret")
 
+        Keyring.getSecret(host, userName)
 
         logger.trace("If you need to debug your output, do it in trace.  Gradle does NOT log trace")
     }
 
     public static boolean setSecret(String host, String userName, String secret) {
         logger.info("Setting secret")
+
+        Keyring.setSecret(host, userName, secret)
+
+        logger.trace("If you need to debug your output, do it in trace.  Gradle does NOT log trace")
+    }
+
+    public static boolean deleteSecret(String host, String userName) {
+        logger.info("Setting secret")
+
+        Keyring.deleteSecret(host, userName)
 
         logger.trace("If you need to debug your output, do it in trace.  Gradle does NOT log trace")
     }
