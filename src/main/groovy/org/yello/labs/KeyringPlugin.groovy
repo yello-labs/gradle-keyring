a8cbe9cca6c97c85ab45fbebe1cb359bc5412a0d
dakota maulding <dakota.maulding@yello-labs.org>
2021-07-30 14:58:36 -0500

    Update to test suite and delimiter

--- src/main/groovy/org/yello/labs/KeyringPlugin.groovy
+++ src/main/groovy/org/yello/labs/KeyringPlugin.groovy
@@ -10,6 +10,9 @@ import org.slf4j.LoggerFactory
 
 import java.nio.charset.Charset
 
+import static org.yello.labs.Const.getDELIMITER
+import static org.yello.labs.Const.getSOURCE_ENV_KEY
+
 /**
  * Implementation of the gradle plugin interface for anything that can call the apply method.
  * Can be used in either the build.gradle or the settings.gradle.
@@ -22,16 +25,17 @@ import java.nio.charset.Charset
  * {@code KeyringPlugin.getSecret(domain,username)}
  */
 
-public class KeyringPlugin implements Plugin {
-    private static final Logger logger = LoggerFactory.getLogger(KeyringPlugin.class);
-    private static boolean sourceFromEnv;
-    private static Dotenv dotenv;
+class KeyringPlugin implements Plugin {
+    private static final Logger logger = LoggerFactory.getLogger(KeyringPlugin.class)
+    private static boolean sourceFromEnv
+    private static Dotenv dotenv
+
     @Override
     void apply(Object parent) {
         KeyringPlugin Keyring = new KeyringPlugin()
 
         if (parent instanceof Project) {
-            Project project = parent;
+            Project project = parent
             project.rootProject.allprojects { prj ->
                 project.ext.keyring = Keyring
             }
@@ -40,20 +44,18 @@ public class KeyringPlugin implements Plugin {
             settings.ext.keyring = Keyring
         }
 
-        sourceFromEnv = parent.hasProperty(Const.SOURCE_ENV_KEY)
-        if(sourceFromEnv){
+        sourceFromEnv = parent.hasProperty(SOURCE_ENV_KEY)
+        if (sourceFromEnv) {
             logger.info('Configuring Dotenv to source from directory {}', parent['rootDir'])
             try {
                 dotenv = Dotenv.configure().ignoreIfMissing().directory(parent['rootDir'].toString()).load()
-            } catch(DotenvException e) {
+            } catch (DotenvException e) {
                 if (e.getMessage().contains("Malformed entry")) {
                     throw new IllegalArgumentException("Failed to load .env file due to malformed entry, try base64 encoding the hostname (echo -n <hostname> | base64) and readd to the .env file")
                 }
-                throw e;
+                throw e
             }
         }
-
-
     }
 
     /**
@@ -63,21 +65,21 @@ public class KeyringPlugin implements Plugin {
      * @param userName : Target username
      * @return the String value returned from the Keyring library
      */
-    public static String getSecret(String host, String userName) {
+    static String getSecret(String host, String userName) {
         logger.info('Retrieving secret')
 
         logger.debug('If you need to debug your output, do it in trace.  Gradle does NOT log trace')
 
         if (sourceFromEnv) {
             logger.info("Sourcing secret information from .env")
-            String pass = dotenv.get(host + "_" + userName)
+            String pass = dotenv.get(host + DELIMITER + userName)
             if (pass != null) {
                 return pass
             }
 
             //TODO: This is not ideal... it's not human readable for simple debugging
-            String encodedUsername = Base64.encoder.encodeToString(host.getBytes(Charset.defaultCharset()));
-            return dotenv.get(encodedUsername+"_"+userName)
+            String encodedUsername = Base64.encoder.encodeToString(host.getBytes(Charset.defaultCharset()))
+            return dotenv.get(encodedUsername + DELIMITER + userName)
         } else {
             return Keyring.getSecret(host, userName)
         }
@@ -91,7 +93,7 @@ public class KeyringPlugin implements Plugin {
      * @param secret : String value of a secret
      * @return boolean to be interpreted as success performing action
      */
-    public static boolean setSecret(String host, String userName, String secret) {
+    static boolean setSecret(String host, String userName, String secret) {
         logger.info("Setting secret")
 
         if (sourceFromEnv){
@@ -111,17 +113,17 @@ public class KeyringPlugin implements Plugin {
      * @param userName : Target username
      * @return boolean to be interpreted as success performing action
      */
-    public static boolean deleteSecret(String host, String userName) {
+    static boolean deleteSecret(String host, String userName) {
         logger.info("Deleting secret")
 
         Keyring.deleteSecret(host, userName)
 
         logger.trace("If you need to debug your output, do it in trace.  Gradle does NOT log trace")
 
-        return true;
+        return true
     }
 
-    static boolean isSourceFromEnv(){
+    static boolean isSourceFromEnv() {
         return sourceFromEnv
     }
 }
